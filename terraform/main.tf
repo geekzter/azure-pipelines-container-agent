601aac49576de8010f861587f5032e5bb8dc4c75
Eric van Wijk <eric@van-wijk.com>
2022-12-27 15:56:54 +0100

    Add output

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,10 +8,13 @@ resource random_string suffix {
 }
 
 locals {
+  agent_identity_client_id     = var.user_assigned_identity_id != "" && var.user_assigned_identity_id != null ? data.azurerm_user_assigned_identity.pre_created_agent_identity.0.client_id : azurerm_user_assigned_identity.agent_identity.0.client_id
+  agent_identity_name          = var.user_assigned_identity_id != "" && var.user_assigned_identity_id != null ? data.azurerm_user_assigned_identity.pre_created_agent_identity.0.name : azurerm_user_assigned_identity.agent_identity.0.name
+  agent_identity_principal_id  = var.user_assigned_identity_id != "" && var.user_assigned_identity_id != null ? data.azurerm_user_assigned_identity.pre_created_agent_identity.0.principal_id : azurerm_user_assigned_identity.agent_identity.0.principal_id
   log_analytics_workspace_resource_id   = var.log_analytics_workspace_resource_id != "" && var.log_analytics_workspace_resource_id != null ? var.log_analytics_workspace_resource_id : module.diagnostics_storage.log_analytics_workspace_resource_id
-  user_assigned_identity_id    = var.user_assigned_identity_id != "" && var.user_assigned_identity_id != null ? var.user_assigned_identity_id : azurerm_user_assigned_identity.agents.0.id
   owner                        = var.application_owner != "" ? var.application_owner : data.azurerm_client_config.default.object_id
   suffix                       = var.resource_suffix != "" ? lower(var.resource_suffix) : random_string.suffix.result
+  user_assigned_identity_id    = var.user_assigned_identity_id != "" && var.user_assigned_identity_id != null ? var.user_assigned_identity_id : azurerm_user_assigned_identity.agent_identity.0.id
   tags                         = merge(
     {
       application              = var.application_name
@@ -36,7 +39,7 @@ resource azurerm_resource_group rg {
   tags                         = local.tags
 }
 
-resource azurerm_user_assigned_identity agents {
+resource azurerm_user_assigned_identity agent_identity {
   name                         = "${azurerm_resource_group.rg.name}-agent-identity"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
@@ -44,10 +47,17 @@ resource azurerm_user_assigned_identity agents {
   count                        = var.user_assigned_identity_id != "" && var.user_assigned_identity_id != null ? 0 : 1
   tags                         = local.tags
 }
+data azurerm_user_assigned_identity pre_created_agent_identity {
+  name                         = element(split("/",var.user_assigned_identity_id),length(split("/",var.user_assigned_identity_id))-1)
+  resource_group_name          = element(split("/",var.user_assigned_identity_id),length(split("/",var.user_assigned_identity_id))-5)
+
+  count                        = var.user_assigned_identity_id != "" && var.user_assigned_identity_id != null ? 1 : 0
+}
+
 resource azurerm_role_assignment agent_storage_contributors {
   scope                        = var.container_registry_id
   role_definition_name         = "AcrPull"
-  principal_id                 = azurerm_user_assigned_identity.agents.0.principal_id
+  principal_id                 = azurerm_user_assigned_identity.agent_identity.0.principal_id
 
   count                        = var.user_assigned_identity_id != "" && var.user_assigned_identity_id != null && var.configure_access_control ? 1 : 0
 }
--- terraform/modules/diagnostics-storage/output.tf
+++ terraform/modules/diagnostics-storage/output.tf
@@ -16,21 +16,12 @@ output diagnostics_storage_name {
   value       = azurerm_storage_account.diagnostics.name
 }
 
-# output diagnostics_storage_share_id {
-#   value       = var.create_files_share ? azurerm_storage_account.share.0.id : null
-# }
-
-# output diagnostics_storage_share_key {
-#   sensitive   = true
-#   value       = var.create_files_share ? azurerm_storage_account.share.0.primary_access_key : null
-# }
-
-# output diagnostics_storage_share_name {
-#   value       = var.create_files_share ? azurerm_storage_account.share.0.name : null
-# }
-
 output diagnostics_share_name {
   value       = var.create_files_share ? azurerm_storage_share.diagnostics.0.name : null
+}
+
+output diagnostics_share_url {
+  value       = var.create_files_share ? azurerm_storage_share.diagnostics.0.url : null
 }
 
 
