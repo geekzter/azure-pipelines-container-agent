3d72c30f836fbe35dbb3be3a1253bb25fc14e2b1
Eric van Wijk <eric@van-wijk.com>
2022-12-28 16:08:03 +0100

    c

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,6 +61,13 @@ resource azurerm_user_assigned_identity agent_identity {
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
 
+  lifecycle {
+    precondition {
+      condition                = var.configure_access_control
+      error_message            = "If user_assigned_identity_id is not set, configure_access_control must be set to true"
+    }
+  }
+
   count                        = var.user_assigned_identity_id != "" && var.user_assigned_identity_id != null ? 0 : 1
   tags                         = local.tags
 }
@@ -76,12 +83,5 @@ resource azurerm_role_assignment agent_registry_access {
   role_definition_name         = "AcrPull"
   principal_id                 = azurerm_user_assigned_identity.agent_identity.0.principal_id
 
-  lifecycle {
-    precondition {
-      condition                = var.configure_access_control
-      error_message            = "If user_assigned_identity_id is not set, configure_access_control must be set to true"
-    }
-  }
-
-  count                        = var.user_assigned_identity_id != "" && var.user_assigned_identity_id != null && var.configure_access_control ? 1 : 0
+  count                        = (var.user_assigned_identity_id == "" || var.user_assigned_identity_id == null) && var.configure_access_control ? 1 : 0
 }
--- terraform/modules.tf
+++ terraform/modules.tf
@@ -9,34 +9,34 @@ module diagnostics_storage {
   tags                         = local.tags
 }
 
-# module container_agents {
-#   source                       = "./modules/container-app"
+module container_agents {
+  source                       = "./modules/container-app"
 
-#   container_image              = var.container_image
-#   container_registry_id        = var.container_registry_id
-#   devops_org                   = var.devops_org
-#   devops_pat                   = var.devops_pat
-#   diagnostics_storage_share_key= module.diagnostics_storage.diagnostics_storage_key
-#   diagnostics_storage_share_name= module.diagnostics_storage.diagnostics_storage_name
-#   diagnostics_share_name       = module.diagnostics_storage.diagnostics_share_name
-#   environment_variables        = local.environment_variables
-#   location                     = var.location
-#   log_analytics_workspace_resource_id   = local.log_analytics_workspace_resource_id
-#   pipeline_agent_cpu           = var.pipeline_agent_cpu
-#   pipeline_agent_memory        = var.pipeline_agent_memory
-#   pipeline_agent_number_max    = var.pipeline_agent_number_max
-#   pipeline_agent_number_min    = var.pipeline_agent_number_min
-#   pipeline_agent_pool_id       = var.pipeline_agent_pool_id
-#   pipeline_agent_pool_name     = var.pipeline_agent_pool_name
-#   pipeline_agent_run_once      = var.pipeline_agent_run_once
-#   pipeline_agent_version_id    = var.pipeline_agent_version_id
-#   resource_group_id            = azurerm_resource_group.rg.id
-#   resource_group_name          = azurerm_resource_group.rg.name
-#   suffix                       = local.suffix
-#   tags                         = local.tags
-#   user_assigned_identity_id    = local.user_assigned_identity_id
+  container_image              = var.container_image
+  container_registry_id        = var.container_registry_id
+  devops_org                   = var.devops_org
+  devops_pat                   = var.devops_pat
+  diagnostics_storage_share_key= module.diagnostics_storage.diagnostics_storage_key
+  diagnostics_storage_share_name= module.diagnostics_storage.diagnostics_storage_name
+  diagnostics_share_name       = module.diagnostics_storage.diagnostics_share_name
+  environment_variables        = local.environment_variables
+  location                     = var.location
+  log_analytics_workspace_resource_id   = local.log_analytics_workspace_resource_id
+  pipeline_agent_cpu           = var.pipeline_agent_cpu
+  pipeline_agent_memory        = var.pipeline_agent_memory
+  pipeline_agent_number_max    = var.pipeline_agent_number_max
+  pipeline_agent_number_min    = var.pipeline_agent_number_min
+  pipeline_agent_pool_id       = var.pipeline_agent_pool_id
+  pipeline_agent_pool_name     = var.pipeline_agent_pool_name
+  pipeline_agent_run_once      = var.pipeline_agent_run_once
+  pipeline_agent_version_id    = var.pipeline_agent_version_id
+  resource_group_id            = azurerm_resource_group.rg.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  suffix                       = local.suffix
+  tags                         = local.tags
+  user_assigned_identity_id    = local.user_assigned_identity_id
 
-#   depends_on                   = [
-#     azurerm_role_assignment.agent_registry_access
-#   ]
-# }
+  depends_on                   = [
+    azurerm_role_assignment.agent_registry_access
+  ]
+}
