b397d7e62d0e015024a68d8b0c7cb5852334e01b
Eric van Wijk <eric@van-wijk.com>
2023-01-14 22:07:49 +0100

    Hide multi-line Terraform output in pipeline

--- scripts/functions.ps1
+++ scripts/functions.ps1
@@ -115,8 +115,14 @@ function Set-PipelineVariablesFromTerraform () {
     foreach ($outputVariable in $json.keys) {
         $value = $json[$outputVariable].value
         if ($value) {
-            # Write variable output in the format a Pipeline can understand
-            Write-Host "##vso[task.setvariable variable=${outputVariable};isOutput=true]${value}"
+            if ($value -notmatch "`n") {
+                $sensitive = $json[$outputVariable].sensitive.ToString().ToLower()
+                # Write variable output in the format a Pipeline can understand
+                "##vso[task.setvariable variable=${0};isOutput=true;issecret={2}]{1}" -f $outputVariable, $value, $sensitive  | Write-Host
+                Write-Host "##vso[task.setvariable variable=${outputVariable};isOutput=true;issecret=]${value}"
+            } else {
+                Write-Verbose "Ignoring multi-line output variable '${outputVariable}'"
+            }
         }
     }
 }
