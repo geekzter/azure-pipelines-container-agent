654a34a187c6be5423a4e7bb22c01890ad2967e8
Eric van Wijk <eric@van-wijk.com>
2023-09-02 14:28:32 +0200

    No ACR private endpoint

--- terraform/modules/network/private-link.tf
+++ terraform/modules/network/private-link.tf
@@ -2,7 +2,7 @@ resource azurerm_private_dns_zone zone {
   for_each                     = var.gateway_type != "NoGateway" ? {
     blob                       = "privatelink.blob.core.windows.net"
     file                       = "privatelink.file.core.windows.net"
-    registry                   = "privatelink.azurecr.io"
+    # registry                   = "privatelink.azurecr.io"
     vault                      = "privatelink.vaultcore.azure.net"
   } : {}
   name                         = each.value
@@ -59,26 +59,26 @@ resource azurerm_private_endpoint diag_blob_storage_endpoint {
 
 # BUG: ACR private endpoint does not work with with Azure Firewall
 #      https://github.com/microsoft/azure-container-apps/issues/892
-resource azurerm_private_endpoint container_registry_endpoint {
-  name                       = "${local.container_registry_name}-endpoint-connection"
-  resource_group_name          = azurerm_virtual_network.pipeline_network.resource_group_name
-  location                     = azurerm_virtual_network.pipeline_network.location
+# resource azurerm_private_endpoint container_registry_endpoint {
+#   name                       = "${local.container_registry_name}-endpoint-connection"
+#   resource_group_name          = azurerm_virtual_network.pipeline_network.resource_group_name
+#   location                     = azurerm_virtual_network.pipeline_network.location
   
-  subnet_id                    = azurerm_subnet.private_endpoint_subnet.0.id
+#   subnet_id                    = azurerm_subnet.private_endpoint_subnet.0.id
 
-  private_dns_zone_group {
-    name                       = azurerm_private_dns_zone.zone["registry"].name
-    private_dns_zone_ids       = [azurerm_private_dns_zone.zone["registry"].id]
-  }
+#   private_dns_zone_group {
+#     name                       = azurerm_private_dns_zone.zone["registry"].name
+#     private_dns_zone_ids       = [azurerm_private_dns_zone.zone["registry"].id]
+#   }
 
-  private_service_connection {
-    is_manual_connection       = false
-    name                       = "${local.container_registry_name}-endpoint-connection"
-    private_connection_resource_id = var.container_registry_id
-    subresource_names          = ["registry"]
-  }
+#   private_service_connection {
+#     is_manual_connection       = false
+#     name                       = "${local.container_registry_name}-endpoint-connection"
+#     private_connection_resource_id = var.container_registry_id
+#     subresource_names          = ["registry"]
+#   }
 
-  tags                         = var.tags
-  count                        = var.gateway_type != "NoGateway" ? 1 : 0
-  depends_on                   = [azurerm_subnet_route_table_association.private_endpoint_subnet]
-}
+#   tags                         = var.tags
+#   count                        = var.gateway_type != "NoGateway" ? 1 : 0
+#   depends_on                   = [azurerm_subnet_route_table_association.private_endpoint_subnet]
+# }
