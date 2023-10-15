6ba874d5003dc43e67e7cd9815abfb8af58cd6a6
Eric van Wijk <eric@van-wijk.com>
2023-10-15 12:20:13 +0200

    stash

--- .devcontainer/createorupdate.ps1
+++ .devcontainer/createorupdate.ps1
@@ -10,4 +10,4 @@ if (!(Test-Path $Profile)) {
     New-Item -ItemType symboliclink -Path $Profile -Target $profileTemplate -Force | Out-Null
 }
 
-terraform -chdir=$(Join-Path (Split-Path $PSScriptRoot -Parent) terraform) init -input=false
+terraform -chdir="$(Join-Path (Split-Path $PSScriptRoot -Parent) terraform)" init -input=false
